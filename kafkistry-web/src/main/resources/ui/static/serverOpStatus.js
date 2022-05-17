b0e9faafb2b208f3e1d2ef05dce040a1c2fc0fab
Antonio Tomac <Antonio.Tomac@infobip.com>
2022-05-17 22:18:10 +0200

    allow operation status box to be queried on container and it's children

--- kafkistry-web/src/main/resources/ui/static/serverOpStatus.js
+++ kafkistry-web/src/main/resources/ui/static/serverOpStatus.js
@@ -1,47 +1,83 @@
 function hideOpStatus() {
-    hideServerOpOnId("server-op-status");
+    hideOpStatusIn($(document));
+}
+
+function hideOpStatusIn(container) {
+    hideServerOpOnIdIn("server-op-status", container);
 }
 
 function showOpProgress(message, preMessage) {
-    showOpProgressOnId("server-op-status", message, preMessage);
+    showOpProgressIn(message, preMessage, $(document));
+}
+
+function showOpProgressIn(message, preMessage, container) {
+    showOpProgressOnIdIn("server-op-status", message, preMessage, container);
 }
 
 function showOpSuccess(message, preMessage) {
-    showOpSuccessOnId("server-op-status", message, preMessage);
+    showOpSuccessIn(message, preMessage, $(document));
+}
+
+function showOpSuccessIn(message, preMessage, container) {
+    showOpSuccessOnIdIn("server-op-status", message, preMessage, container);
 }
 
 function showOpError(message, preMessage) {
-    showOpErrorOnId("server-op-status", message, preMessage);
+    showOpErrorIn(message, preMessage, $(document));
+}
+
+function showOpErrorIn(message, preMessage, container) {
+    showOpErrorOnIdIn("server-op-status", message, preMessage, container);
 }
 
 function showOpProgressOnId(id, message, preMessage) {
-    showOpOnId(id, "progress-status", message, preMessage);
+    showOpProgressOnIdIn(id, message, preMessage, $(document));
+}
+
+function showOpProgressOnIdIn(id, message, preMessage, container) {
+    showOpOnIdIn(id, "progress-status", message, preMessage, container);
 }
 
 function showOpSuccessOnId(id, message, preMessage) {
-    showOpOnId(id, "success-status", message, preMessage);
+    showOpSuccessOnIdIn(id, message, preMessage, $(document));
+}
+
+function showOpSuccessOnIdIn(id, message, preMessage, container) {
+    showOpOnIdIn(id, "success-status", message, preMessage, container);
 }
 
 function showOpErrorOnId(id, message, preMessage) {
-    showOpOnId(id, "error-status", message, preMessage);
+    showOpErrorOnIdIn(id, message, preMessage, $(document));
+}
+
+function showOpErrorOnIdIn(id, message, preMessage, container) {
+    showOpOnIdIn(id, "error-status", message, preMessage, container);
 }
 
 function showOpOnId(id, msgLevelClass, message, preMessage) {
-    hideServerOpOnId(id);
+    showOpOnIdIn(id, msgLevelClass, message, preMessage, $(document));
+}
+
+function showOpOnIdIn(id, msgLevelClass, message, preMessage, container) {
+    hideServerOpOnIdIn(id, container);
     let idSelector = cleanupIdHtmlSelector(id);
-    $("#" + idSelector + " ." + msgLevelClass + " .alert .message").text(message);
-    let preElement = $("#" + idSelector + " ." + msgLevelClass + " .alert .pre-message");
+    container.find("#" + idSelector + " ." + msgLevelClass + " .alert .message").text(message);
+    let preElement = container.find("#" + idSelector + " ." + msgLevelClass + " .alert .pre-message");
     if (preMessage) {
         preElement.text(preMessage);
         preElement.css("display", "block");
     } else {
         preElement.css("display", "none");
     }
-    $("#" + idSelector + " ." + msgLevelClass).css("display", "block");
+    container.find("#" + idSelector + " ." + msgLevelClass).css("display", "block");
 }
 
 function hideServerOpOnId(id) {
-    $("#" + cleanupIdHtmlSelector(id) + " .info").each(function () {
+    hideServerOpOnIdIn(id, $(document));
+}
+
+function hideServerOpOnIdIn(id, container) {
+    container.find("#" + cleanupIdHtmlSelector(id) + " .info").each(function () {
         $(this).css("display", "none");
     });
 }
