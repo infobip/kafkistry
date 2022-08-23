694814a986a691d00b68c6d61b0408e31e434a3c
Antonio Tomac <Antonio.Tomac@infobip.com>
2022-08-23 10:35:36 +0200

    add status type for empty topics

---
+++ kafkistry-service-logic/src/main/kotlin/com/infobip/kafkistry/service/topic/inspectors/TopicEmptyInspector.kt
@@ -0,0 +1,34 @@
+package com.infobip.kafkistry.service.topic.inspectors
+
+import com.infobip.kafkistry.model.ClusterRef
+import com.infobip.kafkistry.model.TopicName
+import com.infobip.kafkistry.service.StatusLevel
+import com.infobip.kafkistry.service.topic.IssueCategory
+import com.infobip.kafkistry.service.topic.TopicExternalInspectCallback
+import com.infobip.kafkistry.service.topic.TopicExternalInspector
+import com.infobip.kafkistry.service.topic.TopicInspectionResultType
+import com.infobip.kafkistry.service.topic.offsets.TopicOffsetsService
+import org.springframework.stereotype.Component
+
+val EMPTY = TopicInspectionResultType(
+    name = "EMPTY",
+    level = StatusLevel.IGNORE,
+    category = IssueCategory.NONE,
+    doc = "Topic has no data in any partition",
+)
+
+@Component
+class TopicEmptyInspector(
+    private val topicOffsetsService: TopicOffsetsService,
+) : TopicExternalInspector {
+
+    override fun inspectTopic(
+        topicName: TopicName,
+        clusterRef: ClusterRef,
+        outputCallback: TopicExternalInspectCallback
+    ) {
+        val empty = topicOffsetsService.topicOffsets(clusterRef.identifier, topicName)?.empty
+            ?: return
+        if (empty) outputCallback.addStatusType(EMPTY)
+    }
+}
