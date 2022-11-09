7955986103a3f1b226f5ad5ba248540a4b9969bf
Antonio Tomac <Antonio.Tomac@infobip.com>
2022-11-09 14:47:31 +0100

    log when test dir is created/destroyed

--- kafkistry-common/src/main/kotlin/com/infobip/kafkistry/utils/test/utils.kt
+++ kafkistry-common/src/main/kotlin/com/infobip/kafkistry/utils/test/utils.kt
@@ -1,12 +1,17 @@
 package com.infobip.kafkistry.utils.test
 
 import org.apache.commons.io.FileUtils
+import org.slf4j.LoggerFactory
 import java.nio.file.Files
+
+private val log = LoggerFactory.getLogger("testFolderFactory")
 
 fun newTestFolder(forWhat: String): String {
     return Files.createTempDirectory("test-tmp-dir-$forWhat-").toFile().also {
-        Runtime.getRuntime().addShutdownHook(Thread {
+        log.info("Created test dir: '{}'", it.absolutePath)
+        Runtime.getRuntime().addShutdownHook(Thread ({
+            log.info("Cleaning test dir: '{}'", it.absolutePath)
             FileUtils.deleteDirectory(it)
-        })
+        }, "ShutdownCleanTestDirHook"))
     }.absolutePath
 }
