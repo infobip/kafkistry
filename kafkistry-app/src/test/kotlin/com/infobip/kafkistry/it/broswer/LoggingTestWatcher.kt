0f79688eb23861f92f4b828eba5d17192231b90d
Antonio Tomac <Antonio.Tomac@infobip.com>
2023-06-27 11:54:11 +0200

    Log browser's console.log

--- kafkistry-app/src/test/kotlin/com/infobip/kafkistry/it/broswer/LoggingTestWatcher.kt
+++ kafkistry-app/src/test/kotlin/com/infobip/kafkistry/it/broswer/LoggingTestWatcher.kt
@@ -2,6 +2,7 @@ package com.infobip.kafkistry.it.broswer
 
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.api.extension.TestWatcher
+import org.openqa.selenium.logging.LogType
 import org.slf4j.LoggerFactory
 import java.util.*
 
@@ -20,6 +21,7 @@ class LoggingTestWatcher : TestWatcher {
         log.info("Test: SUCCESS '{}' - {}",
             context.displayName, context.testClass.map { it.name }.orElse("[null]")
         )
+        logBrowserConsoleLog()
     }
 
     override fun testAborted(context: ExtensionContext, cause: Throwable) {
@@ -27,6 +29,7 @@ class LoggingTestWatcher : TestWatcher {
             context.displayName, context.testClass.map { it.name }.orElse("[null]"),
             cause
         )
+        logBrowserConsoleLog()
     }
 
     override fun testFailed(context: ExtensionContext, cause: Throwable) {
@@ -34,5 +37,15 @@ class LoggingTestWatcher : TestWatcher {
             context.displayName, context.testClass.map { it.name }.orElse("[null]"),
             cause
         )
+        logBrowserConsoleLog()
+    }
+
+    private fun logBrowserConsoleLog() {
+        try {
+            val logEntries = BrowserItTestSuite.chrome.webDriver.manage().logs().get(LogType.BROWSER).all
+            log.info("Got {} browser console log entries", logEntries.size)
+            logEntries.forEachIndexed { index, entry -> println("console.log $index:  $entry") }
+        }  catch (_: Exception) {
+        }
     }
 }
