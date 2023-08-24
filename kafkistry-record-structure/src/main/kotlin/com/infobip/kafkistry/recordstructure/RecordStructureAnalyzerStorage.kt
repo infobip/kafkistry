78f29bfd10bcff4bea8813bf7d008720ccf54a0f
Antonio Tomac <Antonio.Tomac@infobip.com>
2023-08-24 15:27:07 +0200

    fix failure of loading dumper record structures + extra logging

--- kafkistry-record-structure/src/main/kotlin/com/infobip/kafkistry/recordstructure/RecordStructureAnalyzerStorage.kt
+++ kafkistry-record-structure/src/main/kotlin/com/infobip/kafkistry/recordstructure/RecordStructureAnalyzerStorage.kt
@@ -2,9 +2,12 @@ package com.infobip.kafkistry.recordstructure
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.infobip.kafkistry.utils.deepToString
 import org.apache.commons.io.FileUtils
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 import java.io.File
@@ -35,10 +38,13 @@ class InMemoryReferenceRecordStructureAnalyzerStorage : RecordStructureAnalyzerS
 class DirectoryRecordStructureAnalyzerStorage(
     properties: RecordAnalyzerProperties,
 ) : RecordStructureAnalyzerStorage {
+
+    private val log = LoggerFactory.getLogger(DirectoryRecordStructureAnalyzerStorage::class.java)
     private val storage = File(properties.storageDir)
 
     private val objectMapper = ObjectMapper().apply {
         registerKotlinModule()
+        disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
         enable(SerializationFeature.INDENT_OUTPUT)
     }
 
@@ -46,22 +52,26 @@ class DirectoryRecordStructureAnalyzerStorage(
      *  Loads the collected statistic
      */
     override fun load(): ClusterRecordsStructuresMap {
+        log.info("Loading previously dumped record structures from {}", storage)
         if (!storage.exists()) {
+            log.warn("Directory {} is missing, creating...", storage)
             storage.mkdirs()
         }
         val clusterRecordsStructures: ClusterRecordsStructuresMap = ConcurrentHashMap()
         storage.listFiles()?.forEach { kafkaClusterIdDirectory ->
+            log.info("Loading cluster record structures {}", kafkaClusterIdDirectory)
             if (!kafkaClusterIdDirectory.isDirectory) {
+                log.info("Cluster record structures is not directory, skipping. {}", kafkaClusterIdDirectory)
                 return@forEach
             }
-            val clusterTopicsStructures = clusterRecordsStructures
+            val clusterTopicsStructures: RecordsStructuresMap = clusterRecordsStructures
                 .computeIfAbsent(kafkaClusterIdDirectory.name) { ConcurrentHashMap() }
             kafkaClusterIdDirectory.listFiles()?.forEach { topicStructuresFile ->
                 val topicName = topicStructuresFile.name.removeSuffix(".json")
                 try {
                     clusterTopicsStructures[topicName] = readRecordsStructure(topicStructuresFile)
                 } catch (ex: Exception) {
-                    //IGNORE
+                    log.warn("Failed to load topic record structure from {}, skipping it, error={}", topicStructuresFile, ex.deepToString())
                 }
             }
         }
