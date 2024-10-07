8ec19ef366a72a402ecaeb33ccf190473a7634fc
Antonio Tomac <Antonio.Tomac@infobip.com>
2024-10-07 10:55:13 +0200

    Allow marking topic that have behaviour of topic normally being empty, and then do not raise warning status whet it is empty

--- kafkistry-service-logic/src/main/kotlin/com/infobip/kafkistry/service/topic/inspectors/TopicEmptyInspector.kt
+++ kafkistry-service-logic/src/main/kotlin/com/infobip/kafkistry/service/topic/inspectors/TopicEmptyInspector.kt
@@ -1,6 +1,8 @@
 package com.infobip.kafkistry.service.topic.inspectors
 
+import com.infobip.kafkistry.model.Label
 import com.infobip.kafkistry.service.StatusLevel
+import com.infobip.kafkistry.service.existingvalues.ExistingValuesSupplier
 import com.infobip.kafkistry.service.topic.*
 import com.infobip.kafkistry.service.topic.offsets.TopicOffsetsService
 import org.springframework.stereotype.Component
@@ -12,10 +14,24 @@ val EMPTY = TopicInspectionResultType(
     doc = "Topic has no data in any partition",
 )
 
+val EMPTY_OK = TopicInspectionResultType(
+    name = "EMPTY",
+    level = StatusLevel.IGNORE,
+    category = IssueCategory.NONE,
+    doc = "Topic has no data in any partition, and it's normal to be empty",
+)
+
+val EXPECTED_EMPTY_LABEL = Label(
+    category = "BEHAVIOUR",
+    name = "NORMALLY-EMPTY",
+)
+
 @Component
 class TopicEmptyInspector(
     private val topicOffsetsService: TopicOffsetsService,
-) : TopicExternalInspector {
+) : TopicExternalInspector, ExistingValuesSupplier {
+
+    private val advertisedLabels = listOf(EXPECTED_EMPTY_LABEL)
 
     override fun inspectTopic(
         ctx: TopicInspectCtx,
@@ -24,7 +40,13 @@ class TopicEmptyInspector(
         val topicOffsets = ctx.cache { topicOffsetsService.topicOffsets(clusterRef.identifier, topicName) }
             ?: return
         if (topicOffsets.empty) {
-            outputCallback.addStatusType(EMPTY)
+            if (EXPECTED_EMPTY_LABEL in ctx.topicDescription?.labels.orEmpty()) {
+                outputCallback.addStatusType(EMPTY_OK)
+            } else {
+                outputCallback.addStatusType(EMPTY)
+            }
         }
     }
+
+    override fun labels(): List<Label> = advertisedLabels
 }
