d9f0af527c376c70580786d69d819f0dbcd2a4d4
Antonio Tomac <Antonio.Tomac@infobip.com>
2024-11-22 15:30:03 +0100

    Ability to conditionally enable/disable field masking per kafka cluster

--- kafkistry-service-logic/src/main/kotlin/com/infobip/kafkistry/service/consume/TopicDescriptionRecordMaskingRuleProvider.kt
+++ kafkistry-service-logic/src/main/kotlin/com/infobip/kafkistry/service/consume/TopicDescriptionRecordMaskingRuleProvider.kt
@@ -10,13 +10,18 @@ import com.infobip.kafkistry.service.existingvalues.CLASSIFICATION_PII
 import com.infobip.kafkistry.service.existingvalues.CLASSIFICATION_SECRET
 import com.infobip.kafkistry.service.existingvalues.ExistingValuesSupplier
 import com.infobip.kafkistry.service.topic.TopicsRegistryService
+import com.infobip.kafkistry.utils.ClusterFilter
+import com.infobip.kafkistry.utils.ClusterFilterProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.NestedConfigurationProperty
 import org.springframework.stereotype.Component
 
 @Component
 @ConfigurationProperties(prefix = "app.masking.topic-description-fields")
 class TopicDescriptionRecordMaskingRuleProviderProperties {
     var enabled = true
+    @NestedConfigurationProperty
+    var enabledOn = ClusterFilterProperties()
     var classificationsToMask: List<FieldClassification> = listOf(
         CLASSIFICATION_PII, CLASSIFICATION_SECRET,
     )
@@ -28,8 +33,10 @@ class TopicDescriptionRecordMaskingRuleProvider(
     private val topicsRegistryService: TopicsRegistryService,
 ) : RecordMaskingRuleProvider, ExistingValuesSupplier {
 
+    private val clusterFilter = ClusterFilter(properties.enabledOn)
+
     override fun maskingSpecFor(topic: TopicName, clusterRef: ClusterRef): List<TopicMaskingSpec> {
-        if (!properties.enabled) {
+        if (!properties.enabled || !clusterFilter.enabled(clusterRef)) {
             return emptyList()
         }
         val fieldDescriptions = topicsRegistryService.findTopic(topic)
